377e4dd13171585837b279623b790ca5d1120a0f
PavelAnko <pavelanwork@gmail.com>
2022-08-11 14:54:18 +0300

    comp..

---
+++ Fashion/suit.js
@@ -0,0 +1,181 @@
+import React from 'react';
+import './style/suit.css';
+import ShoppingBasketIcon from '@mui/icons-material/ShoppingBasket';
+
+const Suit = () => {
+  return (
+    <div class="suit"> 
+      <div class="loggo">
+        <div class="column-1">
+          <a href='1' id="link" class="column-link-1">Home</a>
+          <a href='1' id="link" class="column-link-1">Shop</a>
+          <a href='1' id="link" class="column-link-1">Lookbook</a>
+          <a href='1' id="link" class="column-link-1">Blog</a>
+          <a href='1' id="link" class="column-link-1">Contact</a>
+        </div>
+        <div class="column-2">Day. DREAM</div>
+        <div class="column-3">
+          <a href='1' id="link" class="column-link-2">Log in</a>
+          <a href='1' id="link" class="column-link-2"><ShoppingBasketIcon /></a>
+        </div>
+      </div>  
+      <div class="title-page">
+        <div class="title-block-1" /> 
+        <div class="title-block-2" />
+        <div class="info-title">
+          <div class="info-1"> Comfy Chic Loungewear </div>
+          <div class="info-2"> I am a paragraph. Click here to add your text </div>
+          <a href='1' class="info-3">Shop Collection</a>
+        </div>
+      </div>
+      <div class="white-t">
+        <div class="block-pick-1">       
+          <div class="picture-1">
+            <a href="1">
+              <div class="href-img-1" />
+            </a>
+          </div>
+          <div class="picture-1-info"> 25% Off Loungewear Sets </div>
+          <a href='1' class="info-4">Shop Now</a>
+        </div>
+        <div class="block-pick-2"> 
+          <div class="picture-2"> 
+            <a href="1">
+              <div class="href-img-2" />
+            </a>
+          </div>
+          <div class="picture-1-info"> Introducing: Comfort Zone </div>
+          <a href='1' class="info-4">Read Our Blog</a>
+        </div>
+      </div>
+      <div class="red-t">
+        <div class="r-1" />
+        <div class="r-2">
+          <div class="bloc-r-1">
+            <div class="r-text-1 ">Day. DREAM  •  LOUNGEWEAR  •  2023 LOOKBOOK </div>  
+          </div>
+          
+          <div class="bloc-r-2">
+            <div class="r-pick" />          
+            <div class="r-text-2">At Home. In Style.</div>
+          </div>
+          <div class="bloc-r-3">
+            <a href='1' class="info-5">View More</a>
+          </div>
+        </div>
+      </div>
+      <div class="bodily-t">
+        <div class="b-1"> 
+          <div class="b-1-text">Follow @DayDream on Instagram</div>
+          <div class="b-1-blocks">
+            <div class="block-b-1">
+              <a href="1" id="link">
+                <div class="href-b"> 
+                  @adima, #adima, #website, #freewebsite #websitemplate, #adima.com
+                </div>
+              </a>
+            </div>
+            <div class="block-b-2">
+              <a href="1" id="link">
+                <div class="href-b"> 
+                  @adima, #adima, #website, #freewebsite #websitemplate, #adima.com
+                </div>
+              </a>
+            </div>
+            <div class="block-b-3">
+              <a href="1" id="link">
+                <div class="href-b"> 
+                  @adima, #adima, #website, #freewebsite #websitemplate, #adima.com
+                </div>
+              </a>
+            </div>
+            <div class="block-b-4">
+              <a href="1" id="link">
+                <div class="href-b"> 
+                  @adima, #adima, #website, #freewebsite #websitemplate, #adima.com
+                </div>
+              </a>
+            </div>
+            <div class="block-b-5">
+              <a href="1" id="link">
+                <div class="href-b"> 
+                  @adima, #adima, #website, #freewebsite #websitemplate, #adima.com
+                </div>
+              </a>
+            </div>
+          </div>
+        </div>
+        <div class="b-2"> 
+          <div class="b-2-text-1">
+            Stay Comfy
+          </div>
+          <div class="b-2-text-2">
+            Subscribe for Updates & Special Offers
+          </div>
+          <div class="b-2-text-3">
+            <input type="email" class="email" placeholder="Email" />
+            <a href='1' class="position-link">
+              <input type="button" class="button" value="Subscribe" />
+            </a>
+          </div>
+        </div>
+        <div class="b-3">
+          <div class="bloc-1-b-3">
+            <div class="b-column-1">
+              <div class="margin-block">
+                <a class="flex-colomn" href='1' id="link">Home</a>
+              </div>
+              <div class="margin-block">
+                <a class="flex-colomn" href='1' id="link">Shop</a>
+              </div>
+              <div class="margin-block">
+                <a class="flex-colomn" href='1' id="link">Blog</a>
+              </div>
+            </div>
+            <div class="b-column-1">
+              <div class="margin-block">
+                <a class="flex-colomn" href='1' id="link">Lookbook</a>
+              </div>
+              <div class="margin-block">
+                <a class="flex-colomn" href='1' id="link">Our Story</a>
+              </div>
+              <div class="margin-block">
+                <a class="flex-colomn" href='1' id="link">Contact</a>
+              </div>
+            </div>
+            <div class="b-column-1">
+              <div class="margin-block">
+                <a class="flex-colomn" href='1' id="link">Store Policy</a>
+              </div>
+              <div class="margin-block">
+                <a class="flex-colomn" href='1' id="link">Shipping & Returns</a>
+              </div>
+              <div class="margin-block">
+                <a class="flex-colomn" href='1' id="link">FAQ</a>
+              </div>
+            </div>
+            <div class="b-column-1">
+              <div class="margin-block">
+                <a class="flex-colomn" href='1' id="link">Facebook</a>
+              </div>
+              <div class="margin-block">
+                <a class="flex-colomn" href='1' id="link">Instagrams</a>
+              </div>
+              <div class="margin-block">
+                <a class="flex-colomn" href='1' id="link">Pinterest</a>
+              </div>
+            </div>
+          </div>
+          <div class="bloc-2-b-3">
+            ©2023 by Day. DREAM.  Proudly created with Adima.com          
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Suit;
+
+
+
