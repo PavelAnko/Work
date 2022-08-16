20dbb0c2dcb3beb61d554e2bdcec38426ec7c70c
PavelAnko <pavelanwork@gmail.com>
2022-08-16 20:58:13 +0300

    finish

---
+++ Fashion/nous.js
@@ -0,0 +1,272 @@
+import React from 'react';
+import './style/nous.css';
+import SearchIcon from '@mui/icons-material/Search';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import LocalMallIcon from '@mui/icons-material/LocalMall';
+
+const Nous = () => {
+  return (
+    <div class="nous">
+      <div class="fixed-b">
+        <div class="fixed-b-1">        
+          <div class="block-text-nous">
+            <a id="link" href=''>
+              <div class="text-nous"> N O U S </div>
+            </a>
+          </div>
+          <a href='' id='link' class="search-nous"><SearchIcon /></a>
+          <input type="search" class="search-text-nous" placeholder="Search..." />
+        </div>
+        <div class="fixed-b-2">
+          <div class="s-w-m-s-a-c">
+            <div class="all-nous">
+              <a id="link" href='' class="style-link-n">Shop All</a>
+            </div>
+            <div class="all-nous">
+              <a id="link" href='' class="style-link-n">Women</a>
+            </div>
+            <div class="all-nous">
+              <a id="link" href='' class="style-link-n">Men</a>
+            </div>
+            <div class="all-nous">
+              <a id="link" href='' class="style-link-n">Sale</a>
+            </div>
+            <div class="all-nous">
+              <a id="link" href='' class="style-link-n">About</a>
+            </div>
+            <div class="all-nous">
+              <a id="link" href='' class="style-link-n">Contact</a>
+            </div>
+          </div>
+          <div class="log-bag">
+            <a href='' id='link' class="account-nous"><AccountCircleIcon fontSize='30px' /></a>
+            <div class="log-in-nous">
+              <a id="link" href='' class="style-log-in-n">Log In</a>
+            </div>
+            <a href='' id='link' class="mall-icon-nous"><LocalMallIcon fontSize='30px' /></a>
+          </div>
+        </div>
+      </div>
+      <div class="invis-tit-f-nous" />
+      <div class="title-nous-1">
+        <div class="title-b-n-1">FREE SHIPPING</div>
+        <div class="title-b-n-2">ON ORDERS OVER $50 - USE COUPON CODE OVER50</div>
+        <div class="title-b-n-3">
+          <a href='/' id='link'>
+            <div class="shop-wms-nous">Shop Women</div>
+          </a>
+          <a href='' id='link'>
+            <div class="shop-wms-nous">Shop Men</div>
+          </a>
+          <a href='' id='link'>
+            <div class="shop-wms-nous">Shop Sale</div>
+          </a>
+        </div>
+      </div> 
+      <div class="photo-3-nous">
+        <div class="photo-n-1" />
+        <div class="photo-n-2" />
+        <div class="photo-n-3" />
+      </div>
+      <div class="new-arri-nous">
+        <div class="flex-text-arri">
+          <div class="text-arri">NEW ARRIVALS</div>
+        </div>
+        <div class="flex-line-arri">
+          <div class="line-arri" />
+        </div>
+      </div>
+      <div class="shop-column">
+        <div class="shop-1-nous shop-pos">
+          <a href='' id="link" class="shop-link-s-nous" />
+          <div class="hover-shop-bloc">
+            <div class="pos-s-b-nous">Quick View</div>
+          </div>
+        </div>
+        <div class="shop-2-nous shop-pos">
+          <a href='' id="link" class="shop-link-s-nous" />
+          <div class="hover-shop-bloc">
+            <div class="pos-s-b-nous">Quick View</div>
+          </div>
+        </div>
+        <div class="shop-3-nous shop-pos">
+          <a href='' id="link" class="shop-link-s-nous" />
+          <div class="hover-shop-bloc">
+            <div class="pos-s-b-nous">Quick View</div>
+          </div>
+        </div>
+        <div class="shop-4-nous shop-pos">
+          <a href='' id="link" class="shop-link-s-nous" />
+          <div class="hover-shop-bloc">
+            <div class="pos-s-b-nous">Quick View</div>
+          </div>
+        </div>
+      </div>
+      <div class="shop-info-nous">
+        <div class="shop-text-nous">
+          <div class="shop-title-nous">I am a product</div>
+          <div class="shop-price-nous">$26.00</div>
+        </div>
+        <div class="shop-text-nous">
+          <div class="shop-title-nous">I am a product</div>
+          <div class="shop-price-nous">$32.00</div>
+        </div>
+        <div class="shop-text-nous">
+          <div class="shop-title-nous">I am a product</div>
+          <div class="shop-price-nous">$32.00</div>
+        </div>
+        <div class="shop-text-nous">
+          <div class="shop-title-nous">I am a product</div>
+          <div class="shop-price-nous">$31.00</div>
+        </div>
+      </div>
+      <div class="shop-column-1">
+        <div class="shop-5-nous shop-pos">
+          <div class="hover-shop-bloc">
+            <div class="pos-s-b-nous">Quick View</div>
+          </div>
+        </div>
+        <div class="shop-6-nous shop-pos">
+          <div class="hover-shop-bloc">
+            <div class="pos-s-b-nous">Quick View</div>
+          </div>
+        </div>
+        <div class="shop-7-nous shop-pos">
+          <div class="bests-seller-nous">Best Seller</div>
+          <div class="hover-shop-bloc">
+            <div class="pos-s-b-nous">Quick View</div>
+          </div>
+        </div>
+        <div class="shop-8-nous shop-pos">
+          <div class="hover-shop-bloc">
+            <div class="pos-s-b-nous">Quick View</div>
+          </div>
+        </div>
+      </div>
+      <div class="shop-info-nous">
+        <div class="shop-text-nous">
+          <div class="shop-title-nous">I am a product</div>
+          <div class="shop-price-nous">$25.00</div>
+        </div>
+        <div class="shop-text-nous">
+          <div class="shop-title-nous">I am a product</div>
+          <div class="shop-price-nous">$26.00</div>
+        </div>
+        <div class="shop-text-nous">
+          <div class="shop-title-nous">I am a product</div>
+          <div class="shop-price-nous">$28.00</div>
+        </div>
+        <div class="shop-text-nous">
+          <div class="shop-title-nous">I am a product</div>
+          <div class="shop-price-nous">$26.00</div>
+        </div>
+      </div>
+      <div class="shop-column-1">
+        <div class="shop-9-nous shop-pos">
+          <div class="bests-seller-nous">Best Seller</div>
+          <div class="hover-shop-bloc">
+            <div class="pos-s-b-nous">Quick View</div>
+          </div>
+        </div>
+        <div class="shop-10-nous shop-pos">
+          <div class="hover-shop-bloc">
+            <div class="pos-s-b-nous">Quick View</div>
+          </div>
+        </div>
+        <div class="shop-11-nous shop-pos">
+          <div class="hover-shop-bloc">
+            <div class="pos-s-b-nous">Quick View</div>
+          </div>
+        </div>
+        <div class="shop-12-nous shop-pos">
+          <div class="hover-shop-bloc">
+            <div class="pos-s-b-nous">Quick View</div>
+          </div>
+        </div>
+      </div>
+      <div class="shop-info-nous">
+        <div class="shop-text-nous">
+          <div class="shop-title-nous">I am a product</div>
+          <div class="shop-price-nous">$25.00</div>
+        </div>
+        <div class="shop-text-nous">
+          <div class="shop-title-nous">I am a product</div>
+          <div class="shop-price-nous">$31.00</div>
+        </div>
+        <div class="shop-text-nous">
+          <div class="shop-title-nous">I am a product</div>
+          <div class="shop-price-nous">$28.00</div>
+        </div>
+        <div class="shop-text-nous">
+          <div class="shop-title-nous">I am a product</div>
+          <div class="shop-price-nous">$28.00</div>
+        </div>
+      </div>
+      <div class="shop-all-nous">
+        <a href='' id='link'>
+          <div class="shop-all-1 flex-shop-nous">Shop All</div>
+        </a>
+        <a href='' id='link'>
+          <div class="shop-all-2 flex-shop-nous">&gt;</div>
+        </a>
+      </div>
+      <div class="additional-information">
+        <div class="d-f-addit">
+          <div class="addit-c">
+            <div class="bloc-addit">
+              <a href='' id='link' class="style-addit">Shop All</a>
+            </div>
+            <div class="bloc-addit">
+              <a href='' id='link' class="style-addit">About</a>
+            </div>
+            <div class="bloc-addit">
+              <a href='' id='link' class="style-addit">Contact</a>
+            </div>
+            <div class="bloc-addit">
+              <a href='' id='link' class="style-addit">Stockists</a>
+            </div>
+          </div>
+          <div class="addit-c-1">
+            <div class="bloc-addit">
+              <a href='' id='link' class="style-addit">FAQ</a>       
+            </div>
+            <div class="bloc-addit">
+              <a href='' id='link' class="style-addit">Shipping & Returns</a>
+            </div>
+            <div class="bloc-addit">
+              <a href='' id='link' class="style-addit">Store Policy</a>
+            </div>
+            <div class="bloc-addit">
+              <a href='' id='link' class="style-addit">Payment Methods</a>
+            </div>
+          </div>
+          <div class="addit-c">
+            <div class="bloc-addit">
+              <a href='' id='link' class="style-addit">Instagram</a>
+            </div>
+            <div class="bloc-addit">
+              <a href='' id='link' class="style-addit">Pinterest</a>
+            </div>
+            <div class="bloc-addit">
+              <a href='' id='link' class="style-addit">Facebook</a>
+            </div>
+            <div class="bloc-addit">
+              <a href='' id='link' class="style-addit">Twitter</a>
+            </div>
+          </div>
+          <div class="addit-c-2">
+            <div class="text-join-nous">Join our mailing list</div>
+            <div class="discount-nous">and get 10% off</div>
+            <input type="email" class="email-nous" placeholder="Enter your email here*" />
+            <a href='' id='link'>
+              <div class="subscribe-nous">Subscribe Now</div>
+            </a>
+          </div>
+        </div>
+      </div>
+      <div class="by-nous">© 2023 by NOUS. Proudly created with Adima.com</div>
+    </div>
+  );
+};
+
+export default Nous;
