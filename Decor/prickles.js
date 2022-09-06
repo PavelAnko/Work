bae3855a696c9761dcecdbaee5e9edd66ba2a5ff
PavelAnko <pavelanwork@gmail.com>
2022-09-06 15:45:35 +0300

    prickles

---
+++ Decor/prickles.js
@@ -0,0 +1,205 @@
+import React from 'react';
+import './style/prickles.css';
+import SearchIcon from '@mui/icons-material/Search';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import LocalMallIcon from '@mui/icons-material/LocalMall';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import TwitterIcon from '@mui/icons-material/Twitter';
+
+const Prickles = () => (
+  <div class='prickles'>
+    <div class='fixed-title-prickles'>
+      <div class='fixed-t-1-prickles'>
+        <div class='search-prickles'>
+          <a href='' id='link' class="icon-search-prickles"><SearchIcon /></a>
+          <input type="search" class="search-text-prickles" placeholder="Search..." />
+        </div>
+        <div class='log-bag-prickles'>
+          <a href='' id='link' class="account-prickles"><AccountCircleIcon /></a>
+          <div class="log-in-prickles">
+            <a id="link" href='' class="style-title-text-prickles">Log In</a>
+          </div>
+          <a href='' id='link' class="mall-icon-prickles"><LocalMallIcon /></a>
+        </div>
+      </div>
+      <div class='fixed-t-2-prickles'>
+        <div class='title-safc-prickles'>
+          <span>
+            <a href="" id="link" class="style-title-prickls">PRICKLES & CO.</a>
+          </span>
+          <div class='s-a-f-c-prickles'>
+            <span>
+              <div class='v-bloc-for-shop-prickles'>
+                <a href="" id="link" class="style-shop-prickls">Shop</a>
+                <div class='bloc-for-shop'>
+                  <a href="" id="link" class="style-for-shop-prickls">Cacti</a>
+                  <a href="" id="link" class="style-for-shop-prickls">Plants</a>
+                  <a href="" id="link" class="style-for-shop-prickls">Succulents</a>
+                </div>
+              </div>
+            </span>
+            <span>
+              <a href="" id="link" class="style-safc-prickls">About</a>
+            </span>
+            <span>
+              <a href="" id="link" class="style-safc-prickls">FAQ</a>
+            </span>
+            <span>
+              <a href="" id="link" class="style-safc-prickls">Contact</a>
+            </span>
+          </div>
+        </div>
+        <div class='info-title-prickles'>We Deliver to Your Doorste</div>
+      </div>
+    </div>
+    <div class='first-block' />
+    <div class='title-for-f-b-prickles'>
+      <span>
+        <div class='bloc-in-title-for-f-1 position-bloc-for-t-f'>
+          <div class='cont-1-fot-bloc-title'>ESTB</div>
+          <div class='cont-2-fot-bloc-title'></div>
+          <div class='cont-1-fot-bloc-title'>2023</div>
+        </div>
+        <div class='bloc-in-title-for-f-2 position-bloc-for-t-f'>PRICKLES & CO</div>
+        <div class='bloc-in-title-for-f-3 position-bloc-for-t-f'>BRING NATURE INDOORS</div>
+        <a href="" id="link">
+          <div class='bloc-in-title-for-f-4 position-bloc-for-t-f'>Shop Now</div>
+        </a>
+      </span>
+    </div>
+    <div class='second-bloc-prickles'>
+      <div class='cacti-s-b-prickles wh-bloc-cps-prickles'>
+        <div class='hw-bloc-cps-prickles'>
+          <a href="" id="link" class='name-bloc-cps-prickles'>
+            CACTI
+          </a>
+        </div>
+        <a href="" id="link">
+          <div class='butt-cps-prickles position-bloc-for-t-f'>Shop Collection</div>
+        </a>
+      </div>
+      <div class='plants-s-b-prickles wh-bloc-cps-prickles'>
+        <div class='hw-bloc-cps-prickles'>
+          <a href="" id="link" class='name-bloc-cps-prickles'>
+            PLANTS
+          </a>
+        </div>
+        <a href="" id="link">
+          <div class='butt-cps-prickles position-bloc-for-t-f'>Shop Collection</div>
+        </a>
+      </div>
+      <div class='succulents-s-b-prickles wh-bloc-cps-prickles'>
+        <div class='hw-bloc-cps-prickles'>
+          <a href="" id="link" class='name-bloc-cps-prickles'>
+            SUCCULENTS
+          </a>
+        </div>
+        <a href="" id="link">
+          <div class='butt-cps-prickles position-bloc-for-t-f'>Shop Collection</div>
+        </a>
+      </div>
+    </div>
+    <div class='third-bloc-prickles'>
+      <div class='half-t-bloc-1-prickles'>
+        <div class='half-t-bloc-info-1-prickles'>
+          <div class='title-half-bloc-info'>
+            <div class='positiont-title-half-prickles'>ABOUT US</div>
+          </div>
+          <div class='info-half-bloc-1-info'>I&#39;m a paragraph. Click here to add your own text and edit me. It’s easy. Just click “Edit Text” or double click me to add your own content and make changes to the font. Feel free to drag and drop me anywhere you like on your page. I’m a great place for you to tell a story and let your users know a little more about you.</div>
+          <div class='info-half-bloc-1-info'>This is a great space to write long text about your company and your services. You can use this space to go into a little more detail about your company. Talk about your team and what services you provide.</div>
+        </div>
+        <div class='half-t-bloc-photo-1-prickles' />
+        <div class='half-t-bloc-photo-2-prickles' />
+      </div>      
+      <div class='half-t-bloc-2-prickles'>
+        <div class='half-t-bloc-photo-3-prickles' />
+        <div class='info-half-bloc-2-info'>
+          <div class='style-info-bloc-2-prickles'>BUY ONLINE NOW & GET 10% OFF !</div>
+          <a href="" id="link">
+            <div class='butt-half-bloc-prickles position-bloc-for-t-f'>Shop Collection</div>
+          </a>
+        </div>
+      </div>        
+    </div>
+    <div class='fixed-bloc-2-prickles'>
+      <div class='regist-bloc-prickles'>
+        <div class='title-regist-bloc-prickles'>FOR SPECIAL REQUESTS & ORDERS</div>
+        <div class='first-last-name-prickles'>
+          <div class='fl-name-bloc-prickles'>
+            <div class='title-contant-prickles'>First Name *</div>
+            <input class='input-text-regist-prickles' type="text" />
+          </div>
+          <div class='fl-name-bloc-prickles'>
+            <div class='title-contant-prickles'>Last Name *</div>
+            <input class='input-text-regist-prickles' type="text" />
+          </div>
+        </div>
+        <div class='e-mail-phone-bloc-prickles'>
+          <div class='title-contant-prickles'>Email *</div>
+          <input class='input-email-phone-regist-prickles' type="email" />
+        </div>
+        <div class='e-mail-phone-bloc-prickles'>
+          <div class='title-contant-prickles'>Phone *</div>
+          <input class='input-email-phone-regist-prickles' type="tel" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" required />
+        </div>
+        <div class='message-bloc-prickles'>
+          <div class='title-contant-prickles'>Message *</div>
+          <textarea class='input-message-regist-prickles' type="text" />
+        </div>
+        <a href="" id="link">
+          <div class='bloc-butt-for-r-prickles position-bloc-for-t-f'>Send</div>
+        </a>
+      </div>
+    </div>
+    <div class='additional-information-prickles'>
+      <div class='addit-bloc-1-prickles'>
+        <div class='cont-bloc-addit-prickles'>
+          <div class='title-addit-contant-prickles'>OUR STORE</div>
+          <div class='info-addit-contant-prickles'>Address: 500 Terry Francois</div>
+          <div class='info-addit-contant-prickles'>Street San Francisco, CA 94158</div>
+          <div class='info-addit-contant-prickles'>Phone: 123-456-7890</div>
+          <div class='info-addit-contant-prickles'>Email:  info@mysite.com</div>
+        </div>
+        <div class='cont-bloc-addit-prickles'>
+          <div class='title-addit-contant-prickles'>OPENING HOURS</div>
+          <div class='info-addit-contant-prickles'>Mon - Fri: 7am - 10pm</div>
+          <div class='info-addit-contant-prickles'>Saturday: 8am - 10pm</div>
+          <div class='info-addit-contant-prickles'>Sunday: 8am - 11pm</div>
+        </div>
+        <div class='cont-bloc-addit-prickles'>
+          <div class='title-addit-contant-prickles'>HELP</div>
+          <div class="position-info-addit-href-prickles">
+            <a href='' class="info-addit-contant-prickles">Shipping & Returns</a>
+          </div>
+          <div class="position-info-addit-href-prickles">
+            <a href='' class="info-addit-contant-prickles">Privacy Policy</a>
+          </div>
+          <div class="position-info-addit-href-prickles">
+            <a href='' class="info-addit-contant-prickles">FAQ</a>
+          </div>
+        </div>
+      </div>
+      <div class='addit-bloc-2-prickles'>
+        <div class='title-addint-bloc-2-prickles'>SUBSCRIBE</div>
+        <div class='but-email-container-addit-prickles'>
+          <div class='bloc-title-email-addit-for-bloc-2-prickles'>
+            <div class='title-bloc-addit-for-bloc-2-prickles'>Enter your email here *</div>
+            <input class='email-bloc-addit-for-bloc-2-prickles' type="email" />
+          </div>
+          <a href="" id="link" class='position-buut-addint-bloc-2-p'>
+            <div class='butt-addit-for-bloc-2-prickles position-bloc-for-t-f'>Subscribe Now</div>
+          </a>
+        </div>
+        <div class='social-networks-icons-prickles'>
+          <a href='' id='link' class="addit-icon-prickles"><InstagramIcon /></a>
+          <a href='' id='link' class="addit-icon-prickles"><FacebookIcon /></a>
+          <a href='' id='link' class="addit-icon-prickles"><TwitterIcon /></a>
+        </div>
+      </div>
+    </div>
+    <div class="by-prickles">© 2023 by PRICKLES. Proudly created with Adima.com</div>
+  </div>
+);
+
+export default Prickles;
